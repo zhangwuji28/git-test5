c855276ecc36114f7b48e16d7c2e997f1d955590
zhangwuji28 <zhangwuji28@163.com>
2024-07-14 18:14:39 +0800

    test

---
+++ git-demo-1/src/main/java/com/zhang/demo/Hello.java
@@ -0,0 +1,13 @@
+package com.zhang.demo;
+
+/**
+ * @ClassName: Hello
+ * @Description:
+ * @Author: zhangwuji28
+ * @Date 2024/7/14 16:40
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
